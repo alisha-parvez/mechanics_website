ed02c33978feaa9861a30940d5c91d27d436cc43
alisha-parvez <64091831+alisha-parvez@users.noreply.github.com>
2020-08-21 14:17:52 +0530

    Add files via upload

---
+++ Responsive_Website/step_5/ex_1/step_5_ex_1.js
@@ -0,0 +1,36 @@
+let check_btn=document.getElementById("check_button");
+let answer_1="8";
+let answer_2="55";
+let answer_3="3";
+let result_section=document.getElementById("result_section");
+let input_box_1=document.getElementById("answer_box_1");
+let input_box_2=document.getElementById("answer_box_2");
+let input_box_3=document.getElementById("answer_box_3");
+
+function check()
+{
+  let user_answer_1=input_box_1.value;
+  let user_answer_2=input_box_2.value;
+  let user_answer_3=input_box_3.value;
+
+  if(user_answer_1===answer_1 && user_answer_2===answer_2 && user_answer_3===answer_3)
+  {
+    input_box_1.style.background="linear-gradient(90deg,#16c75d,#2ded47,#2ded63,#2ded7a)";
+    input_box_2.style.background="linear-gradient(90deg,#16c75d,#2ded47,#2ded63,#2ded7a)";
+    input_box_3.style.background="linear-gradient(90deg,#16c75d,#2ded47,#2ded63,#2ded7a)";
+    result_section.innerHTML="That's Correct!<br>Congratulations...You earned a star<img src='../../res/star_gold.png' class='star'>";
+    result_section.style.display="grid";
+    result_section.style.background="linear-gradient(90deg,#16c75d,#2ded47,#2ded63,#2ded7a)";
+  }
+  else
+  {
+    input_box_1.style.background="linear-gradient(90deg,#d60909,#e82020,#fc4242,#fc5656)";
+    input_box_2.style.background="linear-gradient(90deg,#d60909,#e82020,#fc4242,#fc5656)";
+    input_box_3.style.background="linear-gradient(90deg,#d60909,#e82020,#fc4242,#fc5656)";
+    result_section.innerHTML="Sorry! That's incorrect.<br> Try again or click on the hint or solution buttons";
+    result_section.style.display="grid";
+    result_section.style.background="linear-gradient(90deg,#d60909,#e82020,#fc4242,#fc5656)";
+  }
+}
+
+check_btn.addEventListener("click",check);
---
+++ Responsive_Website/step_5/ex_2/step_5_ex_2.js
@@ -0,0 +1,25 @@
+let check_btn=document.getElementById("check_button");
+let answer="342";
+let result_section=document.getElementById("result_section");
+let input_box=document.getElementById("answer_box");
+
+function check()
+{
+  let user_answer=input_box.value;
+  if(user_answer===answer)
+  {
+    input_box.style.background="linear-gradient(90deg,#16c75d,#2ded47,#2ded63,#2ded7a)";
+    result_section.innerHTML="That's Correct!<br>Congratulations...You earned a star<img src='../../res/star_gold.png' class='star'>";
+    result_section.style.display="grid";
+    result_section.style.background="linear-gradient(90deg,#16c75d,#2ded47,#2ded63,#2ded7a)";
+  }
+  else
+  {
+    input_box.style.background="linear-gradient(90deg,#d60909,#e82020,#fc4242,#fc5656)";
+    result_section.innerHTML="Sorry! That's incorrect.<br> Try again or click on the hint or solution buttons";
+    result_section.style.display="grid";
+    result_section.style.background="linear-gradient(90deg,#d60909,#e82020,#fc4242,#fc5656)";
+  }
+}
+
+check_btn.addEventListener("click",check);
